1aa7cd5846959b6fd7fca4271d0ea9095652e3eb
muromtsev <t4k.org@mail.ru>
2022-01-19 09:47:45 -0900

    create tkinter app

---
+++ tk_app.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+window = Tk()
+window.title('Weather')
+window.geometry('400x250')
+name_city = Label(window, text='Volgograd', font=('monospace', 15))
+name_city.grid(column=0, row=0)
+
+
+
+
+window.mainloop()
